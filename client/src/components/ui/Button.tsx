be4fb55d8a70c005640d26f48b81e6b876c1cd95
Shirsendu Mali <shirsendu1260@gmail.com>
2026-06-07 15:53:37 +0530

    Modify button to have loading and disabled state

--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -1,27 +1,50 @@
 import type { ButtonProps } from '../../types/types.js';
 import { motion } from "motion/react"
+import Spinner from "./Spinner.jsx";
 
 const Button = ({
 	children,
 	className = '',
+	isLoading = false,
+	variant = 'primary',
+	disabled,
 	...props
 }: ButtonProps) => {
+	const variantClasses = {
+		primary: "bg-accent text-white",
+		secondary: "bg-primary text-white",
+		ghost: "border border-border bg-white text-dark",
+	};
+
+	const variantClass = variantClasses[variant] ?? variantClasses.primary;
+
 	return (
 		<motion.button
-			whileHover={{ scale: 1.02 }}
-			whileTap={{ scale: 0.982 }}
+			whileHover={!disabled && !isLoading ? { scale: 1.02 } : undefined }
+			whileTap={!disabled && !isLoading ? { scale: 0.982 } : undefined }
 			transition={{
 				type: "spring",
 				stiffness: 400,
 				damping: 20,
 			}}
+
+			// Prevents user interaction while this button is explicitly disabled or loading state is active
+			// Avoids duplicated API requests
+			disabled={ disabled || isLoading }
+
 			className={`
 				w-full
-				rounded-xl
-				bg-accent
-				font-medium
-				text-white
 				h-12
+				rounded-xl
+				font-medium
+				transition
+				flex
+				items-center
+				justify-center
+				gap-2
+				disabled:cursor-not-allowed
+				disabled:opacity-60
+				${variantClass}
 				${className}
 			`}
 
@@ -29,6 +52,7 @@ const Button = ({
 			{...props}
 		>
 			{children}
+			{isLoading && (<Spinner size="sm" />)}
 		</motion.button>
 	)
 }
--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -1,4 +1,4 @@
-import type { PropsWithChildren, InputHTMLAttributes } from "react";
+import type { PropsWithChildren, InputHTMLAttributes, ReactNode } from "react";
 import type { HTMLMotionProps } from "motion/react";
 
 type CardProps = PropsWithChildren<{
@@ -34,16 +34,41 @@ React already provides these types, so we don't need
 to define every button prop manually.
 */
 
-type ButtonProps = HTMLMotionProps<"button">;
+type ButtonVariant = "primary" | "secondary" | "ghost"; 
+// primary: main actions, secondary: supporting actions, ghost: low priority actions
+
+type ButtonProps = Omit<HTMLMotionProps<"button">, "children"> & {
+	children: ReactNode,
+	isLoading?: boolean,
+	variant?: ButtonVariant
+};
 /*
 HTMLMotionProps<"button"> combines:
-
 1. Native HTML button attributes: type, disabled, onClick, aria-* attributes, etc.
 2. Motion-specific props: whileHover, whileTap, animate, initial, exit, drag, variants, etc.
-
+3. isLoading, variant
 Our Button component renders <motion.button>, not a native <button>.
 Therefore we use HTMLMotionProps instead of ButtonHTMLAttributes
 so that both Motion and native button props are available.
+*/
+/*
+HTMLMotionProps<"button"> allows children to be: ReactNode | MotionValue<number> | MotionValue<string>
+But React expects: ReactNode, inside JSX.
+That's why TypeScript complains at: {children}
+with: Type 'MotionValueNumber' is not assignable to type 'ReactNode'
+Fix with 'Omit':
+Think of it like overriding a TypeScript interface.
+Motion says:
+children: ReactNode | MotionValue<number> | MotionValue<string>;
+We don't want that.
+Our Button should only accept:
+children: ReactNode;
+because:
+<Button>Login</Button>
+or
+<Button><Spinner /></Button>
+are the only valid use cases.
+So we omit the children type provided by motion and add our ReactNode type children
 */
 
 type SectionHeadingProps = PropsWithChildren<{
