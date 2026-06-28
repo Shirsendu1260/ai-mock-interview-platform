ea58ebe684d69d7d6fb02d173fb5f345edcaf73c
Shirsendu Mali <shirsendu1260@gmail.com>
2026-06-28 20:54:48 +0530

    Add get recent interviews endpoint

--- server/src/controllers/dashboard.controller.ts
+++ server/src/controllers/dashboard.controller.ts
@@ -3,8 +3,9 @@ import { ApiError } from '../utils/ApiError.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 import { db } from '../config/db.js';
 import { interviews } from '../db/schema/interviews.js';
-import { eq, sql } from 'drizzle-orm';
+import { eq, sql, desc } from 'drizzle-orm';
 import { interviewFeedbacks } from '../db/schema/interviewFeedbacks.js';
+import { PAGINATION_LIMIT } from '../constants.js';
 
 const getInterviewStats = asyncHandler(async (req, res) => {
     // Auth check
@@ -53,4 +54,38 @@ const getInterviewStats = asyncHandler(async (req, res) => {
     );
 });
 
-export { getInterviewStats };
+const getRecentInterviews = asyncHandler(async (req, res) => {
+    // Auth check
+    if(!req.user) {
+        throw new ApiError(401, 'You need to be authenticated to view your recent interviews.');
+    }
+
+    const authUser = req.user;
+
+
+    // Get a limited number of latest recent interviews
+    const recentInterviews = await db.select({
+                                            id: interviews.id,
+                                            role: interviews.role,
+                                            difficulty: interviews.difficulty,
+                                            qtnsCount: interviews.qtnsCount,
+                                            overallScore: interviewFeedbacks.overallScore,
+                                            completedAt: interviews.completedAt,
+                                            createdAt: interviews.createdAt
+                                        })
+                                        .from(interviews)
+                                        .innerJoin(
+                                            interviewFeedbacks,
+                                            eq(interviewFeedbacks.interviewId, interviews.id)
+                                        )
+                                        .where(eq(interviews.userId, authUser.id))
+                                        .orderBy(desc(interviews.completedAt))
+                                        .limit(PAGINATION_LIMIT);
+
+
+    return res.status(200).json(
+        new ApiResponse(200, recentInterviews, 'Recent interviews fetched successfully.')
+    );
+});
+
+export { getInterviewStats, getRecentInterviews };
--- server/src/routes/dashboard.routes.ts
+++ server/src/routes/dashboard.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getInterviewStats } from '../controllers/dashboard.controller.js';
+import { getInterviewStats, getRecentInterviews } from '../controllers/dashboard.controller.js';
 // import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
@@ -12,6 +12,7 @@ const router = Router();
 ////////////////////////////////  AUTHENTICATED ROUTES  ////////////////////////////////
 
 router.route('/stats').get(verifyJWT, getInterviewStats);
+router.route('/recent-interviews').get(verifyJWT, getRecentInterviews);
 
 
 
