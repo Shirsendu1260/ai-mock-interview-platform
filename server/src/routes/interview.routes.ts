a559b6a07b04d974633f26c893217290de994dbd
Shirsendu Mali <shirsendu1260@gmail.com>
2026-06-29 18:27:18 +0530

    Fix static routes ordering in interview router

--- server/src/routes/interview.routes.ts
+++ server/src/routes/interview.routes.ts
@@ -20,14 +20,14 @@ const router = Router();
 
 ////////////////////////////////  AUTHENTICATED ROUTES  ////////////////////////////////
 
+router.route('/ongoing').get(verifyJWT, getOngoingInterview);
+router.route('/history').get(verifyJWT, getInterviewHistory);
 router.route('/create').post(verifyJWT, upload.single('resume'), createInterview);
 router.route('/:interviewId').get(verifyJWT, getInterview);
 router.route('/:interviewId/questions/:position').get(verifyJWT, getInterviewQuestion);
 router.route('/:interviewId/questions/:position').patch(verifyJWT, saveInterviewQuestionAnswer);
 router.route('/:interviewId/submit').post(verifyJWT, submitInterview);
 router.route('/:interviewId/result').get(verifyJWT, getInterviewResult);
-router.route('/ongoing').get(verifyJWT, getOngoingInterview);
-router.route('/history').get(verifyJWT, getInterviewHistory);
 
 
 
