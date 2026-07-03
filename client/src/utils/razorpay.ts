6fa74592d3492fd9dc74089118c36e88775eee28
Shirsendu Mali <shirsendu1260@gmail.com>
2026-07-03 17:02:23 +0530

    Lazy load Razorpay to initiate checkout

--- client/src/utils/razorpay.ts
+++ client/src/utils/razorpay.ts
@@ -2,8 +2,41 @@ import { APP_NAME } from '../constants/app.js';
 import type { RazorpayOptions } from '../types/razorpay.js';
 import type { IOpenCheckoutProps } from '../types/types.js';
 import largeLogo from '../assets/large-logo.png';
+import { ApiError } from './ApiError.js';
 
-const openRazorpayCheckout = ({
+
+// Loads the Razorpay checkout script dynamically only when payment is needed
+// Avoids downloading Razorpay on every page
+// Returns promise and resolves to 'true' if the script is ready
+const loadRazorpay = (): Promise<boolean> => {
+    // If the script is already loaded, no need to load again
+    if(window.Razorpay) {
+        return Promise.resolve(true); // creates an already-solved promise // Promise<true>
+        // return new Promise((resolve) => resolve(true));
+    }
+
+    // Else load script
+    return new Promise((resolve) => {
+        const script = document.createElement('script');
+
+        // Razorpay checkout script
+        // After this script is loaded in browser, a new global object appears - window.Razorpay
+        // This opens the payment popup
+        script.src = 'https://checkout.razorpay.com/v1/checkout.js';
+
+        // If loaded successfuly
+        script.onload = () => {
+            document.body.appendChild(script); // Add script to HTML body
+            resolve(true);
+        }; // Promise<true>
+
+        // If error occurred
+        script.onerror = () => resolve(false); // Promise<false>
+    });
+}
+
+
+const openRazorpayCheckout = async ({
     orderId,
     amount,
     plan,
@@ -30,13 +63,19 @@ const openRazorpayCheckout = ({
             color: '#2B4C78'
         },
         modal: {
-            ondismiss: () => onDismiss?.(), // ?.() tells TS that if function is availabble, then fire
+            ondismiss: () => onDismiss?.(), // ?.() tells TS that if function is available, then fire
             escape: false, // No close on ESC press
             backdropclose: false, // No close on outside click
             confirm_close: true, // ask before close
             animation: true
         }
     };
+
+    // Lazy load Razorpay script
+    const isRazorpayScriptLoaded = await loadRazorpay(); // now window.Razorpay is available
+    if(!isRazorpayScriptLoaded) {
+        throw new ApiError(500, 'Unable to load Razorpay.');
+    }
 
     // Create Razorpay instance
     const razorpay = new window.Razorpay(options);
@@ -45,4 +84,4 @@ const openRazorpayCheckout = ({
     razorpay.open();
 }
 
-export { openRazorpayCheckout };
+export { loadRazorpay, openRazorpayCheckout };
