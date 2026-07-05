73508113fccc2ad3b06f225d918e2a795b0efeeb
Shirsendu Mali <shirsendu1260@gmail.com>
2026-07-05 20:13:07 +0530

    fix alignment on PageContainer

--- client/src/components/ui/PageContainer.tsx
+++ client/src/components/ui/PageContainer.tsx
@@ -3,12 +3,10 @@ import { LAYOUT } from "../../constants/design.js";
 
 const PageContainer = ({ children }: PageContainerProps) => {
 	return (
-		<div className={`py-7 bg-background ${LAYOUT.paddingX} flex flex-col items-center justify-center w-full`}>
-			<div className={`w-full ${LAYOUT.maxWidth}`}>
-				{children}
-			</div>
+		<div className={`py-7 bg-background ${LAYOUT.paddingX} flex flex-col items-center justify-center`}>
+			{children}
 		</div>
-	);
+	)
 }
 
 // Every page such as Login, Prices, Home etc. will share the same layout.
